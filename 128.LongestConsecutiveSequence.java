56a59df503577547e9587c3e77b6003a6c408680
Shivansh Harbola <92177666+shiviii43@users.noreply.github.com>
2025-08-06 15:19:46 +0530

    Updating

--- 128.LongestConsecutiveSequence.java
+++ 128.LongestConsecutiveSequence.java
@@ -6,11 +6,12 @@ class Solution {
         2.Use two pointers to check if the adjacent elements are forming a subsequence or not.
         3.Keep continuing until the subsequence is maintained.
 
-            Approach 2 : O(n)
-        1.Create a HashMap and insert all the elements into it.
-        2.Insert false with each element into the HashMap.
-        3.Find all the elements which have a potential to start a sequence (If a number-1 exists than that numnber cannot start a sequence) mark them as true.
-        4.    
+        Approach 2 : O(n)
+            1.Create a HashMap and insert all the elements into it.
+            2.Insert false with each element into the HashMap.
+            3.Find all the elements which have a potential to start a sequence (If a number-1 exists than that numnber cannot start a sequence) mark them as true.
+            4.For all the elements that are capable of starting a sequence check how long sequence they can make.
+            5.Keep updating the max with the current sequence.   
         */
         HashMap<Integer,Boolean> hm = new HashMap<>();
         for(int i=0;i<nums.length;i++){
