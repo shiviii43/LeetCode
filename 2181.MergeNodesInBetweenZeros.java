4d1656c8b343d844774e8c08468a33e3cd3b7b1d
Shivansh Harbola <92177666+shiviii43@users.noreply.github.com>
2025-08-28 23:44:12 +0530

    Update 2181.MergeNodesInBetweenZeros.java

--- 2181.MergeNodesInBetweenZeros.java
+++ 2181.MergeNodesInBetweenZeros.java
@@ -18,9 +18,7 @@ class Solution {
             4.Keep iterating until we reach a null node.
             5.At last return the starting of the dummy LL.
 
-            
-        */
-
+        //Code 
         ListNode t = head.next;
         ListNode d = new ListNode(0);
         ListNode ans = d;
@@ -34,6 +32,35 @@ class Solution {
                 d.next=node;
                 d=d.next;
                 sum=0;
+            }
+            t=t.next;
+        }
+        return ans.next;
+            Approach 2 : 
+            Keep a boolean flag to know if we need to : 
+                a.Add the element with the current sum.
+                b.Make a node of the current sum and refresh it.
+
+            Flag = true => add into sum
+            
+        */
+
+        ListNode t = head.next;
+        ListNode d = new ListNode(0);
+        ListNode ans = d;
+        boolean flag = true;
+        int sum = 0;
+        while(t!=null){
+            if(t.val==0) flag=false;
+            if(flag){
+                sum+=t.val;
+            }
+            else{
+                ListNode node = new ListNode(sum);
+                d.next=node;
+                d=d.next;
+                sum=0;
+                flag=true;
             }
             t=t.next;
         }
